a763840d4cf06de267ea56bfe578296c2369bbac
Rashid Carlos Jamil Marcano-Rivera <67828343+RashidCJ@users.noreply.github.com>
2025-09-12 16:41:31 -0400

    Add files via upload

---
+++ Tareas de 6015/Tarea 3-6015.R
@@ -0,0 +1,74 @@
+# SOCI 6015                     #
+# Nombre:                       #
+# Apellidos:                    #
+# Tarea 3: Programación y visualización en R         #
+# Instrucciones ---------------------------------------------------
+
+#En esta tarea harán lo siguiente
+# 1. Guardar esta tarea, cambiando el nombre de Tarea3-6015.R
+# a Apellido1-Apellido2-Tarea2-6015.R 
+# 2. Completar las próximas secciones, guardando la secuencia
+# de comandos que usaron para cumplir con lo pedido. A modo de ejemplo
+# dejo unas partes ya hechas. Continúen desde ahí con sus cambios.
+# Entregarán la secuencia de comandos de R (el script). Completada.
+
+
+# Si contesta una pregunta verbalmente, debe usar el símbolo de #.
+
+
+# Parte 0: Cargar datos ---------------------------------------------------
+# activarán el paquete 'dslabs', así como los paquetes que fueren necesarios
+# por cada tipo de archivo.
+
+library(dslabs) 
+library(tidyverse) # de una vez lo activo porque lo usarán.
+
+# Parte 1: Capítulo 3 -----------------------
+
+#Del libro de Irizarry (2024): Problemas 3.1 y 3.2
+
+#Problema 3.1
+
+#Problema 3.2
+
+# Parte 2: Capítulo 4 ------------------------------------------
+
+
+#Del libro de Irizarry (2024): Problemas 4.13 al 4.18 (por error dice 19). Problema 4.22.
+
+#Problema 4.13
+install.packages("NHANES")
+library(NHANES)
+data("NHANES")
+NHANES|> filter() |> summarise()
+
+#Problema 4.14
+
+#Problema 4.15
+
+#Problema 4.16
+
+#Problema 4.17
+
+#Problema 4.18
+
+#Problema 4.22
+
+# Parte 3: Capítulo 7 -----------------------------------------
+
+#Del libro de Irizarry (2024): Problema 7.1.
+
+
+# Parte 4: Capítulo 8 -----------------------------------------
+
+#Del libro de Irizarry (2024): Problemas 8.17 al 8.20
+
+#Problema 8.17
+
+#Problema 8.18
+
+#Problema 8.19
+
+#Problema 8.20
+
+
